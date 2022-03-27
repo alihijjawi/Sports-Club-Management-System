a5c1e7b219beea86de1ffaddec15faf20f41ea05
yousscharif <97180525+yousscharif@users.noreply.github.com>
2022-03-28 00:07:23 +0300

    User Login and Registration Done 1

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mira@127.0.0.1:3306/users'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+@app.route('/login', methods=['POST'])
+def auth_user():
+    data = request.json
+    user_name = data['user_name']
+    password = data['password']
+    dummy = "d"
+    found = User.query.filter_by(user_name = user_name).first()
+    if found is None:
+        return jsonify({"error": 1, "message" : "Invalid credentials"})
+    if not bcrypt.check_password_hash(found.hashed_password,password):
+        return jsonify({"error": 2, "message": "Invalid credentials"})
+    return jsonify({"error": 0, "message": "Welcome back " + found.first_name+"!"})
+
+
+@app.route('/register', methods=['POST'])
+def create_user():
+    data = request.json
+    email = data['email']
+    user_name = data['user_name']
+    existingUser = User.query.filter_by(user_name=user_name).first()
+    if existingUser is not None:
+        return jsonify({"error": 1, "message": "An account associated with this username already exists."})
+    existingEmail = User.query.filter_by(email=email).first()
+    if existingEmail is not None:
+        return jsonify({"error": 2, "message": "An account associated with this email already exists."})
+    first_name = data['first_name']
+    last_name = data['last_name']
+    password = data['password']
+    date_of_birth = data['dob']
+    user = User(first_name, last_name, user_name, email, date_of_birth, password)
+    db.session.add(user)
+    db.session.commit()
+    message = {"error": 0, "message": "The account has been successfully created."}
+    return jsonify(message)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(128))
+    last_name = db.Column(db.String(128))
+    user_name = db.Column(db.String(128), unique=True)
+    email = db.Column(db.String(128), unique=True)
+    hashed_password = db.Column(db.String(128))
+    date_of_birth = db.Column(db.String(128))
+
+    def __init__(self, first_name, last_name, user_name, email, date_of_birth, password):
+        self.user_name = user_name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+        self.email = email
+        self.hashed_password = bcrypt.generate_password_hash(password)
