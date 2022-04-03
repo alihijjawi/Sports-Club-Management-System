fe9f9d09dae84b5059f799d64c854f518e781841
IssamMisto <iim02@mail.aub.edu>
2022-04-03 16:26:11 +0300

    Coach Login

---
+++ Project---Footer/app.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Fuckapple123123@127.0.0.1:3306/430'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+@app.route('/login', methods=['POST'])
+def auth_user():
+    data = request.json
+    user_name = data['user_name']
+    password = data['password']
+    dummy = "d"
+    found = User.query.filter_by(user_name = user_name).first()
+    if found is None:
+        return jsonify({"error": 1, "message" : "Invalid credentials"})
+    if not (User.query.filter_by(user_name = user_name).first() and User.query.filter_by(password = password)).first():
+        return jsonify({"error": 2, "message": "Invalid credentials"})
+    print("success")
+    return jsonify({"error": 0, "message": "Welcome back " + found.first_name+"!"})
+
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(128))
+    last_name = db.Column(db.String(128))
+    user_name = db.Column(db.String(128), unique=True)
+    email = db.Column(db.String(128), unique=True)
+    password = db.Column(db.String(128))
+    date_of_birth = db.Column(db.String(128))
+
+    def __init__(self, first_name, last_name, user_name, email, date_of_birth, password):
+        self.user_name = user_name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+        self.email = email
+        self.password = password
---
+++ Project---Footer/coach_login_script.js
@@ -0,0 +1,51 @@
+var SERVER_URL = "http://127.0.0.1:5000";
+var errorMessage = document.getElementById("error-message");
+var addButton = document.getElementById("login-button");
+//var resetButton = document.getElementById("reset-button");
+addButton.addEventListener("click", authenticate);
+//resetButton.addEventListener("click", resetInput);
+function resetInput() {
+    const inputFields = document.querySelectorAll("input");
+    for (let i = 0; i < inputFields.length; i++) {
+        inputFields[i].value = "";
+    }
+    errorMessage.innerHTML = "";
+    return;
+}
+function handleResponse(data) {
+    errorMessage.innerHTML = data["message"];
+    return;
+}
+
+function authenticate() {
+    const inputFields = document.querySelectorAll("input");
+    for (let i = 0; i < inputFields.length; i++) {
+        let input = inputFields[i];
+        if (input.value.length == 0) {
+            errorMessage.innerHTML = "Some details are missing! Please fill in all the fields.";
+            return;
+        }
+    }
+    let userName = document.getElementById("user_name")
+    let password = document.getElementById("pwd")
+    const data = {
+        "user_name": userName.value,
+        "password": password.value
+    };
+    fetch(`${SERVER_URL}/login`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(data => handleResponse(data))
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+}
+
+
