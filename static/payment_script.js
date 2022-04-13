66ac43448ba86db50a195728a65991d940dfaf1d
IssamMisto <iim02@mail.aub.edu>
2022-04-13 13:58:56 +0300

    Added error checking

--- static/payment_script.js
+++ static/payment_script.js
@@ -4,6 +4,20 @@ var addButton = document.getElementById("save-button");
 //var resetButton = document.getElementById("reset-button");
 addButton.addEventListener("click", add);
 //resetButton.addEventListener("click", resetInput);
+
+let full_name = document.getElementById("fname")
+let email = document.getElementById("email")
+let address = document.getElementById("adr")
+let city = document.getElementById("city")
+let state = document.getElementById("state")
+let zip_code = document.getElementById("zip")
+let card_name = document.getElementById("cname")
+let card_num = document.getElementById("ccnum")
+let exp_month = document.getElementById("expmonth")
+let exp_year = document.getElementById("expyear")
+let cvv = document.getElementById("cvv")
+
+
 function resetInput() {
     const inputFields = document.querySelectorAll("input");
     for (let i = 0; i < inputFields.length; i++) {
@@ -17,8 +31,80 @@ function handleResponse(data) {
     return;
 }
 
+function ValidateName(input){
+    if (!input.checkValidity()){
+        alert("Please enter a valid Full Name!");
+        console.log("Here 1")
+        return false;
+    }
+    console.log("Here 2")
+    return true;
+}
+
+function ValidateEmail(input){
+    if (!input.checkValidity()){
+        alert("Please enter a valid Email!");
+        return false;
+    }
+    return true;
+}
+
+
+
+function ValidateZip(input){
+    if (!input.checkValidity()){
+        alert("Please enter a valid Zip Code!");
+        return false;
+    }
+    return true;
+}
+
+function ValidateCardNum(input){
+    if (!input.checkValidity()){
+        alert("Please enter a valid Credit Card Number!")
+        return false;
+
+    }
+    return true;
+}
+
+function ValidateMonth(input){
+    const listmonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if (!listmonths.includes(input.value)){
+        alert("Please enter a valid Month!")
+        return false;
+    }
+    return true;
+}
+
+function ValidateYear(input){
+    year = parseInt(input.value, 10);
+    if (year >= 2022 && year <= 2030){
+        return true;
+    }
+    alert("Please enter a valid year")
+    return false;
+}
+
+function ValidateCVV(input){
+    if (!input.checkValidity()){
+        alert("Please enter a valid CVV")
+        return false;
+    }
+
+    return true;
+}
 function add() {
     const inputFields = document.querySelectorAll("input");
+    if (!ValidateName(full_name)) return;
+    if (!ValidateEmail(email)) return;
+    if (!ValidateName(city)) return;
+    if (!ValidateZip(zip_code)) return;
+    if (!ValidateName(card_name)) return;
+    if (!ValidateCardNum(card_num)) return;
+    if (!ValidateMonth(exp_month)) return;
+    if (!ValidateYear(exp_year)) return;
+    if (!ValidateCVV(cvv)) return;
     for (let i = 0; i < inputFields.length; i++) {
         let input = inputFields[i];
         if (input.value.length == 0) {
@@ -26,17 +112,8 @@ function add() {
             return;
         }
     }
-    let full_name = document.getElementById("fname")
-    let email = document.getElementById("email")
-    let address = document.getElementById("adr")
-    let city = document.getElementById("city")
-    let state = document.getElementById("state")
-    let zip_code = document.getElementById("zip")
-    let card_name = document.getElementById("cname")
-    let card_num = document.getElementById("ccnum")
-    let exp_month = document.getElementById("expmonth")
-    let exp_year = document.getElementById("expyear")
-    let cvv = document.getElementById("cvv")
+    
+    
     const data = {
         "full_name": full_name.value,
         "email": email.value,
