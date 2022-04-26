b1c2d02cb0174a0a4e2c14c5502be4aa110ddd78
Mira Awar <msa134@mail.aub.edu>
2022-04-26 20:42:21 +0300

    Sponsorship and Reports Intergated

--- static/submit_form.js
+++ static/submit_form.js
@@ -1,5 +1,53 @@
 var SERVER_URL = "http://127.0.0.1:5000";
 
+var inactivityTime = function () { 
+    var time; 
+    window.onload = resetTimer; 
+    document.onmousemove = resetTimer; 
+    document.onkeydown = resetTimer; 
+    function logout() { window.location.href = "idlelogout" } 
+    function resetTimer() { clearTimeout(time); time = setTimeout(logout, 420000) }
+  };
+  
+  window.onload = function() { inactivityTime(); }
+  
+  var loginButton = document.getElementById("login-button");
+  var logoutButton = document.getElementById("logout-button");
+  var loginDisplay = loginButton.style.display;
+  var logoutDisplay = logoutButton.style.display;
+  var userLabel = document.getElementById("label");
+  var userDisplay = userLabel.style.display;
+  checkLogin(`${SERVER_URL}/checkLogin`);
+  async function checkLogin(url) {
+      const response = await fetch(url);
+      const text = await response.text();
+      try {
+          const data1 = JSON.parse(text); // Try to parse it as JSON
+          // The response was a JSON object
+          // Do your JSON handling here
+          if (data1["found"])
+          {
+              logoutButton.style.display = logoutDisplay;
+              loginButton.style.display = "none";
+              userLabel.innerHTML = "Signed in as " + data1["user_name"];
+              userLabel.style.display = userDisplay;
+              return true;
+          }
+          else
+          {
+              logoutButton.style.display = "none";
+              loginButton.style.display = loginDisplay;
+              userLabel.innerHTML = "";
+              userLabel.style.display = "none";
+              return false;
+          }
+      } catch (err) {
+          // The response wasn't a JSON object
+          // Do your text handling here
+      }
+  }
+  
+
 var submitButton = document.getElementById("submit_button");
 submitButton.addEventListener("click", submit);
 
